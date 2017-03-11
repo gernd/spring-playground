59937d3d0c667f8ea7d6ed9d97daf886aa877a46
Gernot Pointner <gernot.pointner@googlemail.com>
2017-03-11 14:28:13 +0100

    Add configuration

---
+++ configuration/src/main/java/de/gernd/SpringConfigurationApplication.java
@@ -0,0 +1,12 @@
+package de.gernd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigurationApplication.class, args);
+	}
+}
---
+++ configuration/src/test/java/de/gernd/SpringConfigurationApplicationTests.java
@@ -0,0 +1,16 @@
+package de.gernd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringConfigurationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
